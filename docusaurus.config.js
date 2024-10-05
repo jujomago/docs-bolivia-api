9f285efb4e8ca8b36a3697b10d7721539ffdcd83
Juan Josue Mancilla <jujomago@gmail.com>
2024-10-05 01:02:46 -0400

    updated texts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Bolivia Places API",
+  tagline: "The best API for turistic places in Boliva",
   url: "https://jujomago.github.io",
   baseUrl: "/docs-bolivia-api/",
   onBrokenLinks: "throw",
@@ -45,7 +45,7 @@ const config = {
     /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Bolivia API",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -54,61 +54,20 @@ const config = {
           {
             type: "doc",
             docId: "intro",
-            position: "left",
+            position: "right",
             label: "Tutorial",
           },
           { to: "/api", label: "API", position: "left" },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
+            href: "https://github.com/jujomago",
+            label: "My GitHub",
             position: "right",
           },
         ],
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
