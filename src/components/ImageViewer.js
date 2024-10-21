6737c86f9d6a4b04420a594bedaf970614cc118a
Juan Josue Mancilla <jujomago@gmail.com>
2024-10-21 17:08:24 -0400

    added modal when click image, improved title

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,12 +1,29 @@
 import { AsyncImage } from "loadable-image";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ImageViewer = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(0);
+  const dialogRef = useRef(null);
+
+  const handleOpen = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  };
+
+  const handleClose = (e) => {
+    if (dialogRef.current && e.target === dialogRef.current) {
+      dialogRef.current.close();
+    }
+  };
 
   return (
     <div className="image-viewer">
-      <div className="main-image">
+      <div
+        className="main-image"
+        onClick={handleOpen}
+        style={{ cursor: "pointer" }}
+      >
         <AsyncImage
           src={images[selectedImage]}
           alt="Main"
@@ -21,13 +38,37 @@ const ImageViewer = ({ images }) => {
             onClick={() => setSelectedImage(index)}
           >
             <AsyncImage
-              style={{ width: 180, height: 180 }}
+              style={{ width: "100%", height: "100%" }}
               src={image}
               alt={`Thumbnail ${index + 1}`}
             />
           </div>
         ))}
       </div>
+      <dialog
+        ref={dialogRef}
+        style={{
+          width: "auto",
+          maxHeight: "75vh",
+          aspectRatio: "16/10",
+        }}
+        onClick={handleClose}
+      >
+        <AsyncImage
+          src={images[selectedImage]}
+          alt="Full Size"
+          objectFit="cover"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </dialog>
+      <style jsx>{`
+        dialog::backdrop {
+          background: rgb(41 4 4 / 57%);
+        }
+        dialog {
+          border: none;
+        }
+      `}</style>
     </div>
   );
 };
